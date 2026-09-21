3c6a02b4641c27b418837e3470dd450c079a0bcd
Policron Aggabao <pwma08@gmail.com>
2026-09-21 14:40:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# P5JS Interactive Scene
+
+[Rubric and instructions](https://docs.google.com/document/d/1x2r-2b1miJdCSMiBRawdJvanioRUo1c7ARUVJUQDslY/edit)
+
+## Day 1 Planning
+
+## Day 2 Coding
+
+## Day 3 Coding / Create a Presentation
+
+## Day 4 Finalize presentation and watch Presentations
---
+++ sketch.js
@@ -0,0 +1,22 @@
+// Project Title
+// Your Name(s)
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  //This function get run once at the start of the program
+  createCanvas(600, 400);
+  background(240);
+  // ellipseMode(CORNER);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+
+  //Set the number of frames per second
+  frameRate(15);
+}
+
+function draw() {
+  background(220);
+}
